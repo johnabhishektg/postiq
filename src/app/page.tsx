9928a674482722bf883477ff98e34f21d7ee2b05
johnabhishektg <john.tg177@gmail.com>
2025-07-18 23:17:24 +0530

    Linkedin preview

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,54 @@
-import Image from "next/image";
+import LinkedInPreview from "@/components/linkedin-preview";
+import { MaxWidthWrapper } from "@/components/max-width-wrapper";
+import { Navbar } from "@/components/navbar";
+import Sidebar from "@/components/sidebar";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+import { cn } from "@/lib/utils";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="relative h-screen grid grid-cols-3 md:flex-row bg-gray-50 overflow-hidden">
+      {/* <Navbar /> */}
+      {/* sidebar for desktop */}
+      <div className="hidden md:block w-54 lg:w-54 border-r border-gray-100 p-6 h-full text-brand-900 relative z-10">
+        <Sidebar />
+      </div>
+      <LinkedInPreview />
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+      {/* <MaxWidthWrapper className="text-center mt-10 w-1/2 grid gap-4">
+          <h1 className="text-3xl font-medium">What do you want to post?</h1>
+
+          <Textarea
+            placeholder="Enter your prompt here..."
+            className="resize-none mt-4 h-20"
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          <Button className="w-full">Draft Post 🚀</Button>
+
+          <div className="flex items-center justify-between mt-6 ">
+            <div
+              className={cn(
+                "px-3 py-2 text-zinc-600 cursor-pointer rounded-2xl border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50"
+              )}
+            >
+              Write a post on my Series A announcement
+            </div>
+            <div
+              className={cn(
+                "px-3 py-2 text-zinc-600 cursor-pointer rounded-2xl border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50"
+              )}
+            >
+              Help me write a post..{" "}
+            </div>
+            <div
+              className={cn(
+                "px-3 py-2 text-zinc-600 cursor-pointer rounded-2xl border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50"
+              )}
+            >
+              Help me write a post..{" "}
+            </div>
+          </div>
+        </MaxWidthWrapper> */}
     </div>
   );
 }
---
+++ src/components/linkedin-preview.tsx
@@ -0,0 +1,175 @@
+"use client";
+
+import React, { useEffect, useRef, useState } from "react";
+import Image from "next/image";
+import { Icon, Icons } from "./icon";
+import { cn } from "@/lib/utils";
+
+function LinkedInPreview() {
+  const [screenSize, setScreenSize] = React.useState<
+    "mobile" | "tablet" | "desktop"
+  >("desktop");
+  const [processedContent, setProcessedContent] = React.useState<string>(
+    "What do you want to post"
+  );
+  const [isExpanded, setIsExpanded] = useState(false);
+  const [showMoreButton, setShowMoreButton] = useState(false);
+  const contentRef = useRef<HTMLDivElement>(null);
+
+  const checkContentOverflow = () => {
+    setTimeout(() => {
+      const contentElement = contentRef.current;
+      if (contentElement) {
+        const lineHeight = Number.parseInt(
+          window.getComputedStyle(contentElement).lineHeight
+        );
+        const maxHeight = lineHeight * 3;
+        setShowMoreButton(contentElement.scrollHeight > maxHeight);
+      }
+    }, 0);
+  };
+
+  const containerWidth = {
+    mobile: "w-[320px]",
+    tablet: "w-[480px]",
+    desktop: "w-[655px]",
+  };
+
+  return (
+    <div className="flex flex-1 flex-col items-center gap-5 overflow-y-auto bg-gray-50 py-5">
+      <div
+        className={cn(
+          "mx-auto transition-all duration-300",
+          containerWidth[screenSize]
+        )}
+      ></div>
+      <div className="w-[555px] font-system overflow-hidden rounded-lg bg-white shadow ring-1 ring-inset ring-gray-200">
+        <div className="py-5 pl-4 pr-6">
+          <div className="flex items-center gap-3">
+            <div className="min-w-0 flex-1">
+              <div className="flex items-center gap-3">
+                <span className="relative inline-block shrink-0">
+                  <Image
+                    alt=""
+                    loading="lazy"
+                    width={140}
+                    height={140}
+                    className="size-14 rounded-full object-cover"
+                    src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAxMjggMTI4Ij4KICA8cGF0aCBmaWxsPSIjZTdlMmRjIiBkPSJNMCAwaDEyOHYxMjhIMHoiLz4KICA8cGF0aCBkPSJNODguNDEgODQuNjdhMzIgMzIgMCAxMC00OC44MiAwIDY2LjEzIDY2LjEzIDAgMDE0OC44MiAweiIgZmlsbD0iIzc4OGZhNSIvPgogIDxwYXRoIGQ9Ik04OC40MSA4NC42N2EzMiAzMiAwIDAxLTQ4LjgyIDBBNjYuNzkgNjYuNzkgMCAwMDAgMTI4aDEyOGE2Ni43OSA2Ni43OSAwIDAwLTM5LjU5LTQzLjMzeiIgZmlsbD0iIzlkYjNjOCIvPgogIDxwYXRoIGQ9Ik02NCA5NmEzMS45MyAzMS45MyAwIDAwMjQuNDEtMTEuMzMgNjYuMTMgNjYuMTMgMCAwMC00OC44MiAwQTMxLjkzIDMxLjkzIDAgMDA2NCA5NnoiIGZpbGw9IiM1NjY4N2EiLz4KPC9zdmc+Cg=="
+                  />
+                  <span className="absolute bottom-0 right-0 inline-flex size-4 items-center justify-center rounded-full bg-[#1052B8] text-white ring-2 ring-white">
+                    <Icon name="linkedinLogo" className="size-2.5" />
+                  </span>
+                </span>
+                <div className="min-w-0 flex-1">
+                  <p className="truncate text-sm font-semibold text-gray-900">
+                    John Abhishek
+                  </p>
+                  <p className="truncate text-xs font-normal text-gray-500">
+                    Founder @ Prmise
+                  </p>
+                  <div className="flex items-center gap-1">
+                    <span className="text-xs font-normal text-gray-500">
+                      Now
+                    </span>
+                    <span className="text-xs font-normal text-gray-500">•</span>
+                    <Icon
+                      name="linkedInVisibility"
+                      className="size-4 text-gray-500"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+            ref={contentRef}
+            className={cn(
+              "relative mt-5 whitespace-pre-wrap text-sm",
+              !isExpanded && "line-clamp-3"
+            )}
+          >
+            {processedContent}
+          </div>
+          {showMoreButton && (
+            <button
+              onClick={() => setIsExpanded(!isExpanded)}
+              className="mt-2 text-sm font-semibold text-gray-500 hover:text-gray-700"
+            >
+              {isExpanded ? "...less" : "...more"}
+            </button>
+          )}
+        </div>
+        <div className="relative">
+          <div className="overflow-hidden"></div>
+        </div>
+        <div className="py-3 pl-4 pr-6">
+          <div className="flex items-center justify-between">
+            <div className="flex items-center justify-start gap-2">
+              <Image
+                alt="post reactions"
+                loading="lazy"
+                width={24}
+                height={24}
+                className="h-5 w-auto"
+                src="/images/post-reactions.svg"
+              />
+              <span
+                className={cn(
+                  "mt-1 font-medium text-gray-500",
+                  screenSize === "mobile" ? "hidden" : "text-xs"
+                )}
+              >
+                Devv and 88 others
+              </span>
+            </div>
+            <div className="flex items-center justify-end gap-2">
+              {["4 comments", "•", "1 repost"].map((text) => (
+                <span
+                  key={text}
+                  className={cn(
+                    "font-medium text-gray-500",
+                    screenSize === "mobile" ? "text-[10px]" : "text-xs"
+                  )}
+                >
+                  {text}
+                </span>
+              ))}
+            </div>
+          </div>
+          <hr className="mt-3 border-gray-200" />
+          <div
+            className={cn(
+              "mt-2 flex items-center",
+              screenSize === "mobile" ? "justify-between" : "justify-around"
+            )}
+          >
+            {["Like", "Comment", "Share", "Send"].map((action) => (
+              <div
+                key={action}
+                className={cn(
+                  "flex items-center justify-center rounded-lg px-1.5 py-2 font-semibold text-gray-500 hover:bg-gray-100",
+                  screenSize === "mobile"
+                    ? "flex-col text-[10px]"
+                    : "flex-row gap-1.5 text-sm"
+                )}
+              >
+                <Icon
+                  name={`linkedIn${action}` as keyof typeof Icons}
+                  className={cn(
+                    screenSize === "mobile" ? "size-5 mb-1" : "size-5"
+                  )}
+                />
+                <span className={cn(screenSize === "mobile" ? "mt-1" : "")}>
+                  {action}
+                </span>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LinkedInPreview;
